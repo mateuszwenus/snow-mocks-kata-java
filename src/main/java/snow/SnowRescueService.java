6da4d96e16cb2d421d73c91506b244147205f7a5
Mateusz Wenus <mateusz.wenus@gmail.com>
2016-10-28 22:25:00 +0200

    SnowRescueService should throw NPE when WeatherForecastService is null

--- src/main/java/snow/SnowRescueService.java
+++ src/main/java/snow/SnowRescueService.java
@@ -7,6 +7,9 @@ import snow.dependencies.WeatherForecastService;
 public class SnowRescueService {
 
 	public SnowRescueService(WeatherForecastService weatherForecastService, MunicipalServices municipalServices, PressService pressService) {
+		if (weatherForecastService == null) {
+			throw new NullPointerException("weatherForecastService must not be null");
+		}
 	}
 
 	public void checkForecastAndRescue() {
--- src/test/java/snow/SnowRescueServiceTest.java
+++ src/test/java/snow/SnowRescueServiceTest.java
@@ -1,4 +1,31 @@
 package snow;
 
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import snow.dependencies.MunicipalServices;
+import snow.dependencies.PressService;
+
+@RunWith(MockitoJUnitRunner.class)
 public class SnowRescueServiceTest {
+
+	@Mock
+	private MunicipalServices municipalServices;
+	@Mock
+	private PressService pressService;
+
+	@Test
+	public void should_throw_exception_when_WeatherForecastService_is_null() {
+		try {
+			// when
+			new SnowRescueService(null, municipalServices, pressService);
+			fail("should throw NullPointerException");
+		} catch (NullPointerException expected) {
+			// then
+		}
+	}
 }
